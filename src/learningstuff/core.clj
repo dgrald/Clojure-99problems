ac1b75206e755ea20cfda23062d47483916d627d
dgrald <grald.dylan@gmail.com>
2020-01-30 11:18:13 -0700

    Add problems 3 and 4

--- src/learningstuff/core.clj
+++ src/learningstuff/core.clj
@@ -17,6 +17,30 @@
       first-item
       (recur remaining))))
 
+; 3
+(defn kth
+  ([input-list k]
+    (if (> k (count input-list))
+      nil
+      (kth input-list 1 k)))
+  ([input-list c k]
+    (let [[first-item & remaining] input-list]
+      (if (= c k)
+        first-item
+        (recur remaining (+ c 1) k)))))
+
+; 4
+(defn length
+  ([input-list]
+    (if (empty? input-list)
+      0
+      (length input-list 1)))
+  ([input-list l]
+    (let [[first-item & remaining] input-list]
+      (if (empty? remaining)
+        l
+        (recur remaining (+ l 1))))))
+
 ; 5
 (defn reverse-list
   [input-list]
@@ -29,4 +53,4 @@
 (defn -main
   "run examples here"
   [& args]
-  (println (reverse-list [1 2 3 4 5])))
+  (println (length [1])))
